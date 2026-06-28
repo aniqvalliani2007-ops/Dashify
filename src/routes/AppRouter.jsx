efa3b294e77ceb8c5a0722ad380b2293bb71cdb8
AniqValliani <aniqvalliani2007@gmail.com>
2026-06-28 17:00:26 +0500

    aa

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from '../pages/LandingPage'
-import AboutPage from '../pages/AboutPage'
-import BlogPage from '../pages/BlogPage'
-import ArticlePage from '../pages/ArticlePage'
-import ContactPage from '../pages/ContactPage'
 import AdminSubmissionsPage from '../pages/AdminSubmissionsPage'
 import LoginPage from '../pages/LoginPage'
 import RegisterPage from '../pages/RegisterPage'
 import DashboardPage from '../pages/DashboardPage'
-import PricingPage from '../pages/PricingPage'
 import NotFoundPage from '../pages/NotFoundPage'
 import AuthCallbackPage from '../pages/AuthCallbackPage'
 import ProtectedRoute from '../components/auth/ProtectedRoute'
@@ -19,14 +14,9 @@ export const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/blog/:id" element={<ArticlePage />} />
-        <Route path="/contact" element={<ContactPage />} />
         <Route path="/admin/submissions" element={<AdminSubmissionsPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/pricing" element={<PricingPage />} />
         <Route path="/auth/callback" element={<AuthCallbackPage />} />
         <Route 
           path="/dashboard" 
